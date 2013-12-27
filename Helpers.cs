24943d66d7e80805977c9b9b4be5f7bd6caa6375
Wil <wip-@users.noreply.github.com>
2013-12-27 13:56:18 +0900

    Fit a polynomial to a function of multiple parameters (TODO)

---
+++ Helpers.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MultiDimFunctionFitter
+{
+    public static class Helpers
+    {
+        static public int GetBytesPerPixel(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format8bppIndexed:
+                    return 1;
+
+                case PixelFormat.Format24bppRgb:
+                    return 3;
+
+                case PixelFormat.Format32bppArgb:
+                    return 4;
+
+                case PixelFormat.Format64bppArgb:
+                    return 8;
+
+                default:
+                    Debug.Assert(false);
+                    return 0;
+            }
+        }
+
+        static public bool HasAlphaChannel(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format8bppIndexed:
+                case PixelFormat.Format24bppRgb:
+                    return false;
+
+                case PixelFormat.Format32bppArgb:
+                case PixelFormat.Format64bppArgb:
+                    return true;
+
+                default:
+                    Debug.Assert(false);
+                    return false;
+            }
+        }
+
+        static public Color GetColorDiff(Color left, Color right)
+        {
+            int a = Math.Abs(left.A - right.A);
+            int r = Math.Abs(left.R - right.R);
+            int g = Math.Abs(left.G - right.G);
+            int b = Math.Abs(left.B - right.B);
+
+            return Color.FromArgb(
+                Convert.ToByte(a.Clamp0_255()),
+                Convert.ToByte(r.Clamp0_255()),
+                Convert.ToByte(g.Clamp0_255()),
+                Convert.ToByte(b.Clamp0_255()));
+        }
+
+        public static int Clamp0_255(this int value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static float Clamp0_255(this float value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static double Clamp0_255(this double value)
+        {
+            return value.Clamp(0, 255);
+        }
+
+        public static int Clamp0_65535(this int value)
+        {
+            return value.Clamp(0, 65535);
+        }
+
+        //http://stackoverflow.com/a/2683487/758666
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+
+
+        public static double RNormalized(this Color color){ return (double)color.R / 255; }
+        public static double GNormalized(this Color color){ return (double)color.G / 255; }
+        public static double BNormalized(this Color color){ return (double)color.B / 255; }
+
+
+        //http://stackoverflow.com/a/1626175/758666
+        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            double max = Math.Max(color.RNormalized(), Math.Max(color.GNormalized(), color.BNormalized()));
+            double min = Math.Min(color.RNormalized(), Math.Min(color.GNormalized(), color.BNormalized()));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max;
+        }
+
+
+        //http://stackoverflow.com/a/1626175/758666
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
+
+        //// http://en.wikipedia.org/wiki/Luminance_(relative)
+        //// weights each R, G, B component. Sum of params is 1.00
+        //// result is between [0, 1]
+        //// alternative formulas http://stackoverflow.com/a/596243/758666
+        //public static double GetRelativeLuminance(IColor c)
+        //{
+        //    //return 
+        //    //    0.2126 * (double)c.R +
+        //    //    0.7152 * (double)c.G +
+        //    //    0.0722 * (double)c.B;
+
+        //    //return
+        //    //    0.299 * (double)c.R +
+        //    //    0.587 * (double)c.G +
+        //    //    0.114 * (double)c.B;
+
+        //    return Math.Sqrt(
+        //        0.241 * c.RNormalized * c.RNormalized +
+        //        0.691 * c.GNormalized * c.GNormalized +
+        //        0.068 * c.BNormalized * c.BNormalized);
+        //}
+
+
+
+
+        /// <summary>
+        /// Linearly interpolates over the value x between the points (xMin, yMin) and (xMax, yMax).
+        /// </summary>
+        public static double Lerp(
+            double x,
+            double xMin, double xMax,
+            double yMin, double yMax)
+        {
+            double ratio = (x - xMin) / (xMax - xMin);
+            return yMin + ratio * (yMax - yMin);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MultiDimFunctionFitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapInfo bitmapInfoSource;
+        BitmapInfo bitmapInfoFiltered;
+        BitmapInfo bitmapInfoFitted;    // TODO rebuild
+        double[] ri;    // coefficients
+        double[] gi;    // coefficients
+        double[] bi;    // coefficients
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SliderZoomOut_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (SliderZoomOut.Value != 1)
+                Zoom(SliderZoomOut.Value);
+            if (SliderZoomIn != null)
+                SliderZoomIn.Value = 1;
+        }
+
+        private void SliderZoomIn_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (SliderZoomIn.Value != 1)
+                Zoom(SliderZoomIn.Value);
+            if (SliderZoomOut != null)
+                SliderZoomOut.Value = 1;
+        }
+
+        private void ButtonResetZoom_Click(object sender, RoutedEventArgs e)
+        {
+            Zoom(1);
+            SliderZoomIn.Value = 1;
+            SliderZoomOut.Value = 1;
+        }
+
+        private void MyCatch(System.Exception ex)
+        {
+            var st = new StackTrace(ex, true);      // stack trace for the exception with source file information
+            var frame = st.GetFrame(0);             // top stack frame
+            String sourceMsg = String.Format("{0}({1})", frame.GetFileName(), frame.GetFileLineNumber());
+            Console.WriteLine(sourceMsg);
+            MessageBox.Show(ex.Message + Environment.NewLine + sourceMsg);
+            Debugger.Break();
+        }
+
+        private void Zoom(double val)
+        {
+            try
+            {
+                var myScaleTransform = new System.Windows.Media.ScaleTransform();
+                myScaleTransform.ScaleY = val;
+                myScaleTransform.ScaleX = val;
+                if (LabelZoom != null)
+                    LabelZoom.Content = val;
+                var myTransformGroup = new System.Windows.Media.TransformGroup();
+                myTransformGroup.Children.Add(myScaleTransform);
+
+                System.Windows.Controls.Image[] images =
+                    new System.Windows.Controls.Image[] { ImageSource, ImageFiltered };
+
+                foreach (System.Windows.Controls.Image image in images)
+                {
+                    if (image == null || image.Source == null)
+                        continue;
+                    //image.RenderTransform = myTransformGroup;
+                    image.LayoutTransform = myTransformGroup;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MyCatch(ex);
+            }
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            double vert = ((ScrollViewer)sender).VerticalOffset;
+            double hori = ((ScrollViewer)sender).HorizontalOffset;
+
+            ScrollViewer[] scrollViewers = new ScrollViewer[] { ScrollViewerSource, ScrollViewerFiltered };
+
+            foreach (ScrollViewer scrollViewer in scrollViewers)
+            {
+                scrollViewer.ScrollToVerticalOffset(vert);
+                scrollViewer.ScrollToHorizontalOffset(hori);
+                scrollViewer.UpdateLayout();
+            }
+        }
+
+        private void ImageSource_MouseMove(object sender, MouseEventArgs e)
+        {
+            Image_MouseMove(ImageSource, e);
+        }
+
+        private void ImageFiltered_MouseMove(object sender, MouseEventArgs e)
+        {
+            Image_MouseMove(ImageFiltered, e);
+        }
+
+        private void Image_MouseMove(System.Windows.Controls.Image clickedImage, MouseEventArgs e)
+        {
+            int x = (int)(e.GetPosition(clickedImage).X);
+            int y = (int)(e.GetPosition(clickedImage).Y);
+
+            BitmapInfo[] bitmapInfos =
+                new BitmapInfo[] { bitmapInfoSource, bitmapInfoFiltered };
+
+            System.Windows.Controls.Label[] labels =
+                new System.Windows.Controls.Label[] { LabelColorSource, LabelColorFiltered };
+
+            LabelInfo.Content = String.Format("X={0:D4}, Y={1:D4}", x, y);
+
+            for (int i = 0; i < 2; ++i)
+            {
+                if (bitmapInfos[i] == null) continue;
+
+                System.Drawing.Color color = bitmapInfos[i].GetPixelColor(x, y);
+                float hue = color.GetHue();
+                labels[i].Content = String.Format("A={0:D3}, R={1:D3}, G={2:D3}, B={3:D3}, H={4:###.##}",
+                    color.A, color.R, color.G, color.B, hue);
+            }
+
+
+            // TODO show in LabelColorFitted the value of interpolated surface
+            if (ri!=null)
+            {
+                Color colorIn = bitmapInfoSource.GetPixelColor(x, y);
+                Color colorFitted = GetFittedColor(colorIn);
+                float hue = colorFitted.GetHue();
+                LabelColorFitted.Content = String.Format("A={0:D3}, R={1:D3}, G={2:D3}, B={3:D3}, H={4:###.##}",
+                    colorFitted.A, colorFitted.R, colorFitted.G, colorFitted.B, hue);
+            }
+        }
+
+
+        private Color GetFittedColor(Color colorIn)
+        {
+            double r1 = colorIn.RNormalized();
+            double g1 = colorIn.GNormalized();
+            double b1 = colorIn.BNormalized();
+            //double d = : TODO density
+
+            double r2 = r1 * r1;
+            double g2 = g1 * g1;
+            double b2 = b1 * b1;
+            //double d2 = d * d;
+
+            double r3 = r1 * r2;
+            double g3 = g1 * g2;
+            double b3 = b1 * b2;
+            //double d3 = d * d2;
+
+
+            double r = ri[0] * r3 + ri[1] * r2 + ri[2] * r1 + ri[3] * g3 + ri[4] * g2 + ri[5] * g1 + ri[6] * b3 + ri[7] * b2 + ri[8] * b1 + ri[9] * 1;
+            double g = gi[0] * r3 + gi[1] * r2 + gi[2] * r1 + gi[3] * g3 + gi[4] * g2 + gi[5] * g1 + gi[6] * b3 + gi[7] * b2 + gi[8] * b1 + gi[9] * 1;
+            double b = bi[0] * r3 + bi[1] * r2 + bi[2] * r1 + bi[3] * g3 + bi[4] * g2 + bi[5] * g1 + bi[6] * b3 + bi[7] * b2 + bi[8] * b1 + bi[9] * 1;
+
+            return Color.FromArgb(255, (byte)(r * 255), (byte)(g * 255), (byte)(b * 255));
+        }
+
+
+        private void ImageSource_Drop(object sender, DragEventArgs e)
+        {
+            String msg = LoadImage(ImageSource, out bitmapInfoSource, e);
+
+            if (msg != null)
+                LabelInfo.Content = msg;
+            else
+            {
+                UpdateCoefficients();
+            }
+        }
+
+        private void ImageFiltered_Drop(object sender, DragEventArgs e)
+        {
+            String msg = LoadImage(ImageFiltered, out bitmapInfoFiltered, e);
+
+            if (msg != null)
+                LabelInfo.Content = msg;
+            else
+            {
+                UpdateCoefficients();
+            }
+        }
+
+        private static Bitmap LoadBitmap(String filename, out String errorMessage)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                if (fs != null)
+                    fs.Close();
+                errorMessage = "File already in use!";
+                return null;
+            }
+
+            Bitmap bitmap;
+            //try
+            {
+                bitmap = new Bitmap(fs);
+                errorMessage = null;
+            }
+            //catch (System.Exception /*ex*/)
+            //{
+            //    bitmap.Dispose();
+            //    errorMessage = "Not an image!";
+            //}
+            return bitmap;
+        }
+
+        private String LoadImage(
+            System.Windows.Controls.Image destinationImage,
+            out BitmapInfo destinationBitmapInfo,
+            DragEventArgs e)
+        {
+            destinationBitmapInfo = null;
+
+            try
+            {
+                if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                    return "Not a file!";
+
+                String[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 1)
+                    return "Too many files!";
+
+                String imageSourceFileName = files[0];
+
+                if (!File.Exists(imageSourceFileName))
+                    return "Not a file!";
+
+                String errorMessage;
+                Bitmap destinationBitmap = LoadBitmap(imageSourceFileName, out errorMessage);
+                if (errorMessage != null)
+                    return errorMessage;
+
+                destinationImage.Source =
+                    Imaging.CreateBitmapSourceFromHBitmap(
+                        destinationBitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, 
+                        System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                destinationBitmapInfo = new BitmapInfo(destinationBitmap);
+
+                return null;
+            }
+            catch (System.Exception ex)
+            {
+                MyCatch(ex);
+                return "Exception";
+            }
+        }
+
+        private void UpdateCoefficients()
+        {
+            if (bitmapInfoSource == null || bitmapInfoFiltered == null)
+                return;
+
+            int width = bitmapInfoSource.Width;
+            int height = bitmapInfoSource.Height;
+            int pixelsCount = width * height;
+
+            double[] Rinputs3 = new double[pixelsCount];
+            double[] Ginputs3 = new double[pixelsCount];
+            double[] Binputs3 = new double[pixelsCount];
+            double[] Dinputs3 = new double[pixelsCount]; // we will need density too
+            
+            double[] Rinputs2 = new double[pixelsCount];
+            double[] Ginputs2 = new double[pixelsCount];
+            double[] Binputs2 = new double[pixelsCount];
+            double[] Dinputs2 = new double[pixelsCount]; // we will need density too
+
+            double[] Rinputs1 = new double[pixelsCount];
+            double[] Ginputs1 = new double[pixelsCount];
+            double[] Binputs1 = new double[pixelsCount];
+            double[] Dinputs1 = new double[pixelsCount]; // we will need density too
+
+            double[] Cinputs0 = new double[pixelsCount];
+
+            double[] Routputs = new double[pixelsCount];
+            double[] Goutputs = new double[pixelsCount];
+            double[] Boutputs = new double[pixelsCount];
+
+            int counter = 0;
+
+            for (int x = 0; x < width; ++x )
+            for (int y = 0; y < height; ++y )
+            {
+                System.Drawing.Color colorIn = bitmapInfoSource.GetPixelColor(x, y);
+                System.Drawing.Color colorOut = bitmapInfoFiltered.GetPixelColor(x, y);
+
+                double r = colorIn.RNormalized();
+                double g = colorIn.GNormalized();
+                double b = colorIn.BNormalized();
+                //double d = sample id
+
+                double r2 = r*r;
+                double g2 = g*g;
+                double b2 = b*b;
+                //double d2 = d*d;
+
+                Rinputs3[counter] = r*r2;
+                Ginputs3[counter] = g*g2;
+                Binputs3[counter] = b*b2;
+                //Dinputs3[counter] = ; // TODO
+
+                Rinputs2[counter] = r2;
+                Ginputs2[counter] = g2;
+                Binputs2[counter] = b2;
+                //Dinputs2[counter] = ; // TODO
+
+                Rinputs1[counter] = r;
+                Ginputs1[counter] = g;
+                Binputs1[counter] = b;
+                //Dinputs1[counter] = ; // TODO
+
+                Cinputs0[counter] = 1;
+
+                Routputs[counter] = colorOut.RNormalized();
+                Goutputs[counter] = colorOut.GNormalized();
+                Boutputs[counter] = colorOut.BNormalized();
+
+                ++counter;
+            }
+
+
+            List<double[]> columns = new List<double[]>
+            {
+                Rinputs3,
+                Ginputs3,
+                Binputs3,
+                //Dinputs3,
+                            
+                Rinputs2,
+                Ginputs2,
+                Binputs2,
+                //Dinputs2,
+                            
+                Rinputs1,
+                Ginputs1,
+                Binputs1,
+                //Dinputs1,
+
+                Cinputs0
+            };
+
+            // http://christoph.ruegg.name/blog/linear-regression-mathnet-numerics.html
+            var X = DenseMatrix.OfColumns(pixelsCount, 10, columns);           
+            var Yred = new DenseVector(Routputs);
+            var Ygreen = new DenseVector(Goutputs);
+            var Yblue = new DenseVector(Boutputs);
+
+            try
+            {
+                var QR = X.QR();
+
+                ri = QR.Solve(Yred).ToArray();
+                gi = QR.Solve(Ygreen).ToArray();
+                bi = QR.Solve(Yblue).ToArray();
+            }
+            catch (System.Exception ex)
+            {
+                MyCatch(ex);	
+            }
+
+        }
+
+    }
+}
